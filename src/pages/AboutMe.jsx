6ff306b66c687a71c9fbcfda3b222edfd0c65a89
omarbramirez <omar.ramirez94@hotmail.es>
2024-06-16 01:56:35 -0600

    info complemented with CV

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -3,81 +3,58 @@ import Profile from '../assets/images/profile.jpg'
 function AboutMe (){
     return (
         <>
-      <div className="col-12" style={{ marginTop: "100px" }}></div>
+      <div className="col-12"></div>
 
-      <div className="about-slide1 col-6 col-md-10 text-display">
-        <div>
-          <h1>Programador de profesión, diseñador como vocación y humanista por convicción.</h1>
-          <p>
-            <br />Sería fácil pensar que mi pequeño proyecto como profesional independiente fue un accidente de los drásticos cambios sufridos durante el 2020. Pero estaría mintiendo. Lo cierto es que, aunque precipitada, esta reorientación era algo insoslayable.
-            <br /><br />Empecemos con lo más básico. Estudié en la facultad de Filosofía y Letras de mi ciudad (Puebla, México) y obtuve mi título hace algún tiempo (2018). Sin embargo, terminé de formarme fuera del aula. En humanidades todos alrededor te dicen exactamente lo mismo; "para ti hay solo tres caminos: el posgrado, la investigación, o la docencia". Por mucho, tres actividades con las que nunca me identifiqué. Mi alternativa era clara, la única forma de abrirme una cuarta opción vendría de la educación en línea.
+      <div className="about-slide1 col-12 col-md-10 text-display" style={{ marginTop: "100px", marginBottom: "100px"}}>
+        <div style={{ marginTop: "100px"}}>
+          <h1 style={{color: "var(--primary-5)"}}>Desarrollador web [ Marzo, 2020 – 2024]</h1>
+          <p style={{ marginTop: "50px" }}>
+            <h2>Secretaría de Cultura de Quito [Febrero, 2024 - Actualmente]            </h2>
+            <br />Desarrollé una web aplicando metodología SCRUM debido a las necesidades institucionales del proyecto. Diseñé toda la interfaz con el enfoque mobile-first. Usé React como librería base. Tailwind  y Next.Js en su PMV; y Vite para la versión final. 
+          </p>
+
+          <p style={{ marginTop: "50px" }}>
+            <h2>Valeria Adalid (cliente particular) [Diciembre, 2023 – Marzo, 2024]          </h2>
+            <br />Desarrollé una aplicación web aplicando TDD básico. Usé las APIs de Paypal y Google para generar funcionalidades automatizadas de seguridad de datos, e-comerce y gestión de horario. Basé mi implementación en NodeJs, usando VanillaJs para presentar un PMV; y Vite para la versión final. 
+          </p>
+
+          <p style={{ marginTop: "50px" }}>
+            <h2>Valeria Guzmán (cliente particular) [Mayo, 2023 – Enero,  2024]    </h2>
+            <br />Migré el frontend de una web (previamente desarrollada con VanillaJs, HTML y CSS) a React, usando Zustand para la gestión de estados. Dada la poca flexibilidad y altos costos que tenía el alojamiento de esta web, decidí hacer una migración a otras plataformas con implementación JAMstack como Netlify.
+          </p>
+        </div>
+
+        <div style={{ marginTop: "100px" }}>
+          <h1 style={{color: "var(--primary-5)"}}>Diseñador digital [Septiembre, 2018 – Diciembre, 2023]          </h1>
+          <p style={{ marginTop: "50px" }}>
+            <h2>NOBILIS Café [Octubre, 2023 – Noviembre, 2024]           </h2>
+            <br />Creé la identidad visual de una marca cafetera.  Realicé un estudio de mercado para comprender las tendencias del sector y los competidores más próximos. Diseñé el logotipo  garantizando su escalabilidad a otras áreas comerciales y su aplicación análoga (uniformes, flyers, empaques) y digital (redes sociales, web, apps). Estandaricé la documentación y los formatos: manuales, archivos editables, etc.
+
+          </p>
+          <p style={{ marginTop: "50px" }}>
+            <h2>Osa de Anteojos [Junio, 2022 – Diciembre, 2022]          </h2>
+            <br />Adapté un libro físico para niños a su versión digital animada usando las características del formato Epub 0.3. Usé Adobe InDesign y Calibre como softwares de diseño y gestión de elementos gráficos respectivamente.
+          </p>
+        </div>
+
+        <div style={{ marginTop: "100px" }}>
+          <h1 style={{color: "var(--primary-5)"}}>Formación</h1>
+          <p style={{ marginTop: "50px" }}>
+            <h2>UNAM | Ingeniería en Computación [2022]
+            </h2>
+          </p>
+          <p style={{ marginTop: "50px" }}>
+            <h2>BUAP | Licenciatura en Lingüística [2019]
+            </h2>
+          </p>
+          <p style={{ marginTop: "50px" }}>
+            <h2>Facultad de Lenguas (BUAP) | Examen de certificación de Nivel C1 en inglés [2017]
+            </h2>
           </p>
         </div>
       </div>
 
-      <div className="about-slide1B col-6 col-md-10 text-display">
-        <div>
-          <img src={Profile} alt="Perfil" />
-          <p>
-            <br /><br />En estos días soy un desarrollador web con 3 años de experiencia creando páginas y aplicaciones web para distintos perfiles profesionales. Experto en JavaScript, NodeJs y REACT. Usuario regular de: Java, Git, Sass, PHP, MySQL, MongoDB y TypeScript.
-            <br /><br />Como community manager he colaborado internacionalmente en la Feria Internacional del Libro de Quito (2020 - 2022 respectivamente), y para el proyecto multimedia: Mujeres al Oído (con más de 80,000 reproducciones en Youtube).
-            <br /><br />Siendo diseñador digital me he apoyado de la Suite de Adobe para crear la imagen corporativa de múltiples negocios particulares. Así como para producir animaciones en algunos proyectos editoriales independientes.
-            <br /><br />Cuando no estoy estudiando o programando me gusta dibujar, aprender de filosofía, leer comics y jugar en Steam. Estoy aprendiendo árabe.
-            <br />
-            <br />
-          </p>
-        </div>
-      </div>
 
-      <div className="col-12" style={{ marginTop: "50px" }}></div>
-
-      {/* <!--+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ FIN SECCION UNO -->
-
-      <!--+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++SECCION DOS --> */}
-
-      <div className="col-12" style={{ marginTop: "100px" }}></div>
-
-      {/* <!--+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++FIN SECCION DOS -->
-
-      <!--+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++SECCION TRES --> */}
-
-      {/* <!-- <div className="about-slide3 col-12 col-md-10 text-display">
-        <div>
-          <h1>FORMACIÓN</h1>
-
-          <h2><br>The Complete 2022 Web Development Bootcamp: Dr. Angela Yu</h2>
-          <p>Certificación digital obtenida.</p>
-
-          <h2><br>Responsive Web Desing Certification: Free Code Camp</h2>
-          <p>Certificación digital obtenida.</p>
-
-          <h2><br> Introducción al desarrollo web con PHP: Ignacio Cruz Moreno</h2>
-          <p>Certificación digital obtenida.</p>
-
-          <h2><br>The Complete JavaScript Course 2022, From Zero to Expert!: Jonas Schmedtmann</h2>
-          <p>Certificación digital obtenida.</p>
-
-          <h2><br>Become a UX Designer: Brendan Bolton-Klinger</h2>
-          <p>Certificación digital obtenida.</p>
-
-          <h2><br>Node.js, Express, MongoDB & More, The Complete Bootcamp 2022: Jonas Schmedtmann</h2>
-          <p>Certificación digital obtenida.</p>
-
-          <br />
-          <h2>Otros</h2>
-
-          <h2><br>Licenciatura en Lingüística y Literatura Hispánica: Benemérita Universidad Autónoma de Puebla </h2>
-          <p> Título obtenido.</p>
-
-          <h2><br>TOEFL ITP B2 CEFR Level (2018)</h2>
-          <p>Certificación obtenida.</p>
-
-          <h2><br>The English with Lucy Business English Course: Lucy Bella Earl</h2>
-          <p>Certificación obtenida.</p>
-
-          <br />
-        </div>
-      </div> --> */}
 
       {/* <!--+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++FIN SECCION TRES --> */}
         </>
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -4,12 +4,12 @@ import Profile from '../assets/images/profile.jpg'
 function Index ({setPage}){
     return(
         <>
-        <div className="col-12" style={{ marginTop: "100px" }}></div>
+        <div className="col-12" style={{ marginTop: "100px"  }}></div> 
   
-        <div className="home-slide col-6 col-md-10 text-display">
+        <div className="home-slide col-6 col-md-10 text-display" >
           <h1>¡Hola, soy Omar!</h1>
           <p>
-            <br />Desarrollador web con 4 años de experiencia. Experto en JavaScript, NodeJs y REACT.
+            <br />Desarrollador web y diseñador digital con 4 años de experiencia trabajando de forma remota para múltiples clientes nacionales e internacionales. Experto en JavaScript, NodeJs y REACT; usuario regular de Git, Mongoose y TypeScript.
           </p>
           <a onClick={()=>{setPage("about-me")}}>
             <button className="button1">Saber más</button>
@@ -39,62 +39,16 @@ function Index ({setPage}){
         <div className="home-slide2B col-6 col-md-10" style={{}}>
           <h1>Servicios:</h1>
           <p>
-            <br />
-            Creación de páginas profesionales.<br />
-            <br />Desarrollo de aplicaciones web.<br />
-            <br />Producción de contenido digital para redes sociales.<br />
-            <br />Diseño de logotipos.<br />
-            <br />Ilustración digital.<br />
+            <br />Desarrollo de Aplicaciones Web y Móviles.<br />
+            <br />Optimización de Performance y SEO.<br />
+            <br />Mantenimiento y Soporte Aplicaciones.<br />
+            <br />Consultoría y Auditoría de Código.<br />
+            <br />Producción de Contenido Digital.<br />
+            <br />Diseño Profesional de Logotipos.<br />
+
           </p>
         </div>
-  
-       {/* <div className="col-12" style={{ marginTop: "100px" }}></div>
-  
-        <div className="background col-12">
-          <div className="section4 col-12 text-jumbo">
-          LO QUE MIS CLIENTES OPINAN
-          </div>
-  
-           <div className="slideropinion col-12" style={{ minHeight: "700px" }}>
-            <ul>
-              <li>
-                <img src="assets/images/review01.png" alt="Opinión 1" />
-                <div className="texto-slider">
-                <h1>Valeria Guzmán</h1>
-                  <h2>Escritora profesional</h2>
-                  <p>
-                  El trabajo de Omar es impecable. Sus ideas son frescas e innovadoras, pero siempre trabaja con conocimiento de causa. Me ha ayudado con el manejo de mi cuenta profesional de Instagram, la creación de contenido para mi editorial y para el salón de la poesía de la Feria Internacional del Libro de Quito. Asimismo, me ayudó a crear y publicar mi página web con absoluta comprensión de los múltiples roles profesionales en los que me desenvuelvo. ¡Estoy muy agradecida de trabajar con él!
-                  </p>
-                </div>
-              </li>
-              <li>
-              <img src="assets/images/review02.png" alt="Opinión 2" />
-              <div className="texto-slider">
-                  <h1>Valeria Adalid</h1>
-                  <h2>Nutrióloga especialista</h2>
-                  <p>
-                  El trabajo de Omar para mi marca personal resultó todo un éxito. Gracias a su análisis para mi marca, él logró desarrollarme un logo con el cual me sentí identificada; ahora ese logo ya es parte de mi imagen en redes sociales. Actualmente se encuentra elaborando una aplicación de reservación automatizada de consultas para mi página web, por lo que me siento muy emocionada del resultado final ¡Gracias por el excelente trabajo!!
-                  </p>
-                  </div>
-                  </li>
-                  <li>
-                <img src="assets/images/review03.png" alt="Opinión 3" />
-                <div className="texto-slider">
-                <h1>Azuany Fuentes</h1>
-                <h2>Odontóloga</h2>
-                <p>
-                    Excelente resultado de traducción, respetando el orden e imágenes (formato) en general.
-                    
-                    El tiempo de entrega fue antes de lo esperado, estoy totalmente satisfecha con la calidad de su trabajo.
-                    Así mismo siempre mostró mucho profesionalismo y disponibilidad sobre alguna duda que me pudiera surgir con respecto a los resultados de mi formato. ¡1000% recomendado!
-                    </p>
-                    </div>
-                    </li>
-                    </ul>
-                    </div> 
-                </div>*/}
-  
-        <div className="finalone col-12" style={{ height: "100px" }}></div>
+        <div className="col-12" style={{ marginTop: "100px"  }}></div> 
     </>
     )
 }
